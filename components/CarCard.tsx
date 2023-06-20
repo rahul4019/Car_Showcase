570f7c054f6cbe3694d6a9ba14b4885f8489806f
rahul4019 <rahulkashyap4019@gmail.com>
2023-06-20 12:09:29 +0530

    Add details and view more button to car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,6 +13,7 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+  const [IsOpen, setIsOpen] = useState(false);
   const carRent = calculateCarRent(city_mpg, year);
   return (
     <div className="car-card group">
@@ -34,6 +35,38 @@ const CarCard = ({ car }: CarCardProps) => {
           priority
           className="object-contain"
         />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === 'a' ? 'Automatic' : 'Manual'}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tyre.svg" width={20} height={20} alt="tyre" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
     </div>
   );
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,17 @@
 'use client';
 
-import { CustomButtonProps } from '@/types';
 import Image from 'next/image';
+
+import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({
   title,
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  isDisabled,
+  rightIcon,
 }: CustomButtonProps) => {
   return (
     <button
@@ -16,7 +20,17 @@ const CustomButton = ({
       onClick={handleClick}
       type={btnType || 'button'}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
