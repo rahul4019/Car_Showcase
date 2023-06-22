371645d87bdf9951eabb2889f8946a4c66a14b0e
rahul4019 <rahulkashyap4019@gmail.com>
2023-06-22 20:35:47 +0530

    Fix TypeScript errors of .env variables

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { CarProps, FilterProps } from '@/types';
 export async function fetchCars(filters: FilterProps) {
   const { manfacturer, year, limit, model, fuel } = filters;
   const headers = {
-    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY,
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY || '',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
   const res = await fetch(
@@ -40,7 +40,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   url.searchParams.append(
     'customer',
-    process.env.NEXT_PUBLIC_IMAGIN_STUDIO_KEY
+    process.env.NEXT_PUBLIC_IMAGIN_STUDIO_KEY || ''
   );
   url.searchParams.append('make', make);
   url.searchParams.append('modelFamily', model.split(' ')[0]);
